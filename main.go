a3b837c9e03ff0efee84ea4bcf58d7b02618c53b
 Guilherme Beckham <guilhermebeckham14@hotmail.com>
2026-01-07 19:36:50 -0300

    feat: adding viper to config env

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 	"log"
 	"simple_bank/api"
 	db "simple_bank/db/sqlc"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:root@localhost:5432/simple_bank?sslmode=disable"
-	address  = "0.0.0.0:8080"
+	"simple_bank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -22,7 +22,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err = server.Start(address); err != nil {
+	if err = server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server", err)
 	}
 }
