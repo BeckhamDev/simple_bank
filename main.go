42e2b604cb2bf4749c7c8672dc8904ea752b5d4f
 Guilherme Beckham <guilhermebeckham14@hotmail.com>
2026-01-07 17:22:32 -0300

    feat: setup gin http server

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
-package simplebank
+package main
 
-func main() {}
+import (
+	"database/sql"
+	"log"
+	"simple_bank/api"
+	db "simple_bank/db/sqlc"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:root@localhost:5432/simple_bank?sslmode=disable"
+	address  = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	if err = server.Start(address); err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
