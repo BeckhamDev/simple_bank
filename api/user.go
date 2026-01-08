1df9ae39324ecbb28853d0053ca848333132ae9f
 Guilherme Beckham <guilhermebeckham14@hotmail.com>
2026-01-07 23:38:17 -0300

    feat: user CRUD routes

---
+++ api/user.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"net/http"
+	db "simple_bank/db/sqlc"
+	"simple_bank/util"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required,min=6"`
+	Fullname string `json:"fullname" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req CreateUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashPass, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashPass,
+		FullName:       req.Fullname,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
